321095de08c574abc0e629793d07d9c00bcf02a4
yerin03 <kyr2532@soongsil.ac.kr>
2023-11-17 22:45:02 +0900

    feat: 독서모임의 책 리스트를 반환한다

--- src/main/java/com/goormthon/agoragoormthon/readingBook/application/ReadingBookService.java
+++ src/main/java/com/goormthon/agoragoormthon/readingBook/application/ReadingBookService.java
@@ -1,18 +1,24 @@
 package com.goormthon.agoragoormthon.readingBook.application;
 
 
+import com.goormthon.agoragoormthon.book.domain.Book;
+import com.goormthon.agoragoormthon.book.dto.BookListDto;
+import com.goormthon.agoragoormthon.book.repository.BookRepository;
 import com.goormthon.agoragoormthon.readingBook.domain.ReadingBook;
 import com.goormthon.agoragoormthon.readingBook.dto.request.ReadingBookRequest;
 import com.goormthon.agoragoormthon.readingBook.repository.ReadingBookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ReadingBookService {
 
     private final ReadingBookRepository readingBookRepository;
+    private final BookRepository bookRepository;
 
     public void saveReadingBook(ReadingBookRequest request) {
         ReadingBook readingBook = ReadingBook.builder()
@@ -20,6 +26,23 @@ public class ReadingBookService {
                 .bookClubId(request.getBookClubId())
                 .build();
         readingBookRepository.save(readingBook);
+    }
+
+    public List<BookListDto> getBooksByBookClubId(Long bookClubId) {
+
+        List<ReadingBook> readingBooks = readingBookRepository.findByBookClubId(bookClubId);
+
+
+        List<Long> bookIds = readingBooks.stream()
+                .map(ReadingBook::getBookId)
+                .collect(Collectors.toList());
+
+        List<Book> books = bookRepository.findAllById(bookIds);
+
+        return books.stream()
+                .map(book -> new BookListDto(book.getId(), book.getTitle(), book.getAuthors(), book.getImgUrl()))
+                .collect(Collectors.toList());
+
     }
     
 }
--- src/main/java/com/goormthon/agoragoormthon/readingBook/repository/ReadingBookRepository.java
+++ src/main/java/com/goormthon/agoragoormthon/readingBook/repository/ReadingBookRepository.java
@@ -4,9 +4,11 @@ import com.goormthon.agoragoormthon.readingBook.domain.ReadingBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReadingBookRepository extends JpaRepository<ReadingBook, Long> {
 
-
+    List<ReadingBook> findByBookClubId(Long bookClubId);
 
 }
