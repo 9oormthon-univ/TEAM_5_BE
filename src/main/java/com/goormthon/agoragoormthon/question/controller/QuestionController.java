9ee7cf4c3176d944c04758e4fc4ef23e3da469c5
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2023-11-18 22:12:51 +0900

    Fix: 발제 리스트 조회 경로 수정

--- src/main/java/com/goormthon/agoragoormthon/question/controller/QuestionController.java
+++ src/main/java/com/goormthon/agoragoormthon/question/controller/QuestionController.java
@@ -33,8 +33,8 @@ public class QuestionController {
         return ResponseType.createSuccessWithData(questionService.deleteQuestion(questionId));
     }
 
-    @GetMapping("/{readingBookId}")
-    public ResponseType<List<QuestionInfoResponse>> getQuestionList(@PathVariable("readingBookId") Long readingBookId) {
+    @GetMapping("/list")
+    public ResponseType<List<QuestionInfoResponse>> getQuestionList(@RequestParam("readingBookId") Long readingBookId) {
         return ResponseType.createSuccessWithData(questionService.getQuestionList(readingBookId));
     }
 }
