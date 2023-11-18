58f7efcf8c9a22902b50cd9f04fb25902211954e
yerin03 <kyr2532@soongsil.ac.kr>
2023-11-18 21:00:08 +0900

    feat: 출판사 추가

---
+++ src/main/java/com/goormthon/agoragoormthon/book/dto/BookSearchDto.java
@@ -0,0 +1,21 @@
+package com.goormthon.agoragoormthon.book.dto;
+
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookSearchDto {
+    private Long id;
+    private String title;
+    private String authors;
+    private String imgUrl;
+    private String publisher;
+
+}
+
+
